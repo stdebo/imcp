8facdc7cc3187b138b6fd534273018c5a086e5de
stdebo <stdebo@163.com>
2020-05-26 11:53:57 +0800

    创建项目基础结构

---
+++ imcp-objects/imcp_objects.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 127.0.0.1
+Source Server Version : 50711
+Source Host           : localhost:3306
+Source Database       : imcp_objects
+
+Target Server Type    : MYSQL
+Target Server Version : 50711
+File Encoding         : 65001
+
+Date: 2020-05-26 11:28:31
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
