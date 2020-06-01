8f7c82d0f306b22f8b3a6791c0a8e1f2ef81e510
stdebo <stdebo@163.com>
2020-06-02 01:04:00 +0800

    创建项目基础结构

---
+++ imcp-consumer/src/test/java/com/imcp/objects/ImcpObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imcp.objects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImcpObjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
